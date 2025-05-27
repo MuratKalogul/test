ec52da5e87697c64ac9fb2cb4cbb0935001916bc
MuratKalogul <muratkalogul@gmail.com>
2025-05-27 03:34:39 +0200

    Update düzenli.js

--- düzenli.js
+++ düzenli.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use('/', createProxyMiddleware({
-    target: 'https://example.com',
+    target:https://1wbfqv.life/',
     changeOrigin: true,
     pathRewrite: { '^/': '/' }
 }));
